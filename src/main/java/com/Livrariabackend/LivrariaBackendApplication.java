5e69815df47d6eb1367274df2e9b02529c338dec
crrsj <crrsj1@gmail.com>
2024-01-09 18:07:13 -0300

    iniciando o projeto

---
+++ src/main/java/com/Livrariabackend/LivrariaBackendApplication.java
@@ -0,0 +1,13 @@
+package com.Livrariabackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LivrariaBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LivrariaBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/Livrariabackend/LivrariaBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Livrariabackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LivrariaBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
