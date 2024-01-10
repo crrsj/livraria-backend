3edfc096f5f15a46a4003676479e002a9e1682e0
crrsj <crrsj1@gmail.com>
2024-01-10 11:03:09 -0300

    ajustes no perfil dev

--- src/main/java/com/Livrariabackend/Config/DevConfig.java
+++ src/main/java/com/Livrariabackend/Config/DevConfig.java
@@ -2,6 +2,7 @@ package com.Livrariabackend.Config;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 
@@ -17,6 +18,7 @@ public class DevConfig {
 	@Value("${spring.jpa.hibernate.ddl-auto}")
 	private String strategy;
 	
+	@Bean
 	public boolean instanciaBaseDeDados() {
 		if(strategy.equals("create")) {
 			this.dbService.instanciaBaseDeDados();
